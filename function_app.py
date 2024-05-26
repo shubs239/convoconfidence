4b515201bc57adbd7c9f56ea4c735d3d348593a3
shubham singh <shubs239@gmail.com>
2024-05-27 02:20:00 +0530

    Added python function azure APIs

---
+++ function_app.py
@@ -0,0 +1,58 @@
+import azure.functions as func
+import logging
+from openai import OpenAI
+import json
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+@app.route(route="convoConfidenceMessage", methods=['POST'])
+def convoConfidenceMessage(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    try:
+        req_body = req.get_json()
+        user_input = req_body.get('user_input')
+        chat_history = req_body.get('chat_history')
+        print(user_input)
+    except ValueError as e:
+        logging.error(f'Error parsing request body: {e}')
+        return func.HttpResponse(
+            "Invalid request body",
+            status_code=400
+        )
+
+    if user_input and chat_history is not None:
+        deepinfra_api_key = ""
+        openai = OpenAI(
+            api_key=deepinfra_api_key,
+            base_url="https://api.deepinfra.com/v1/openai",
+        )
+        
+        messages = chat_history + [{"role": "user", "content": user_input}]
+        
+        try:
+            chat_completion = openai.chat.completions.create(
+                model="meta-llama/Meta-Llama-3-8B-Instruct",
+                messages=messages,
+            )
+            #reply = chat_completion.choices[0].message.content
+            response_data = {
+                "reply": "Hello, how are you?",
+                "usage": {
+                    "prompt_tokens": 10,#chat_completion.usage.prompt_tokens,
+                    "completion_tokens": 10#chat_completion.usage.completion_tokens
+                }
+            }
+            return func.HttpResponse(json.dumps(response_data), status_code=200, mimetype="application/json")
+        except Exception as e:
+            logging.error(f"Deepinfra API request failed: {e}")
+            return func.HttpResponse(
+                f"Deepinfra API request failed: {e}",
+                status_code=500
+            )
+    else:
+        logging.error('Missing user_input or chat_history in the request body')
+        return func.HttpResponse(
+            "Please provide user_input and chat_history in the request body.",
+            status_code=400
+        )
